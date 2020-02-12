00eafc800e8c75a9b728486304276ed4208d71e6
Tarek Aouadhi <tarekaouadhi@gmail.com>
2020-02-12 15:39:36 +0100

    Projektdateien hinzufügen.

---
+++ singeleton/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singeleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automobil m = Autofabrik.MachEines();
+            m.Name = "Tillmanns";
+            Automobil x = Autofabrik.MachEines();
+       
+
+            Console.WriteLine(m.Name);
+            Console.WriteLine(m.GetHashCode());
+            Console.WriteLine(x.Name);
+            Console.WriteLine(x.GetHashCode());
+            Console.ReadLine();
+        }
+    }
+    class Automobil
+    {
+        public string Name { get; set; }
+    }
+    class Autofabrik
+    {
+        private static Automobil automobil = null;
+        public static Automobil MachEines()
+        {
+            if (automobil == null)
+            {
+                automobil = new Automobil();
+            }
+            return automobil; 
+        }
+    }
+}
